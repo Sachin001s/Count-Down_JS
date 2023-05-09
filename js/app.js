c03031dc1d0fbc88c7cad2c694d51bda6cf7f0ce
sachin <sachinofficial823@gmail.com>
2023-05-09 11:09:37 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,36 @@
+const endDate = "5 April 2023 10:00 AM";
+document.getElementById('end-date').innerText=endDate;
+
+const inputs = document.querySelectorAll("input");
+
+const clock = ()=>
+{
+    const end = new Date(endDate);
+    const now = new Date();
+
+    // convert ms to second
+
+    const diff = (end - now) / 1000;
+
+    // no negative value comes:-
+    if(diff<0) return;
+
+    // convert into days
+    inputs[0].value = Math.floor(diff /3600 /24);
+
+    // convert into hours
+    inputs[1].value = Math.floor(diff / 3600) % 24;
+
+     // convert into minutes
+     inputs[2].value = Math.floor(diff / 60) % 60;
+
+    // convert into seconds
+      inputs[3].value = Math.floor(diff  % 60);
+
+}
+clock();
+
+setInterval(()=>
+{
+    clock();
+},1000)
